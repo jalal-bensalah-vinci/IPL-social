b669470b7f23c169d5e74b375f0c155589d3020c
jalal0753 <jalalben0753@gmail.com>
2026-01-16 16:30:54 +0100

    add last tests for the mailChecker function

--- tests/emailChecker.test.ts
+++ tests/emailChecker.test.ts
@@ -5,4 +5,23 @@ describe("Test de la fonction mailChecker",() => {
         expect(mailChecker("abcgmail.com")).toBe(false);
     });
 
+    test("Email ne contenant pas de point dans le nom de domaine: email invalide",() =>{
+        expect(mailChecker("abc@gmailcom")).toBe(false);
+    });
+
+    test("Email contenant des espace: email invalide",() =>{
+        expect(mailChecker("abc @ gmail.com")).toBe(false);
+    });
+
+    test("Email ne contient pas de texte avant le '@': email invalide",() =>{
+        expect(mailChecker("@gmail.com")).toBe(false);
+    });
+
+    test("Email ne contient pas de texte après le '@': email invalide",() =>{
+        expect(mailChecker("abc@")).toBe(false);
+    });
+
+    test("Email valide",() =>{
+        expect(mailChecker("abc@gmail.com")).toBe(true);
+    });
 });
