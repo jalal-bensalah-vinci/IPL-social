c50631f3b41ef211499f67c62319a86b19dbc943
jalal0753 <jalalben0753@gmail.com>
2026-01-16 17:15:36 +0100

    Refactor: optimize email validation logic in emailChecker function

--- src/emailChecker.ts
+++ src/emailChecker.ts
@@ -8,19 +8,17 @@ function emailChecker(mail : string) : boolean{
     }
 
     const parts = mail.split("@");
-    const domain = parts[1];
-
-    if(!domain.includes(".")){
-        return false;
-    }
-
-    if(domain.endsWith(".")){
-        return false;
-    }
 
     if(parts[0].length == 0){
         return false;
     }
+
+    const domain = parts[1];
+
+    if(!domain.includes(".") || domain.endsWith(".")){
+        return false;
+    }
+
 
     return true;
 }
